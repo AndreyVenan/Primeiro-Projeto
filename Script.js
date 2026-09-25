0f097d520c4fdf1768dd049e7a9a7134e9b580fc
Dvnn <69130146+AndreyVenan@users.noreply.github.com>
2026-09-25 12:13:29 -0300

    JS para tema escuro e claro
    Tema escuro e claro

---
+++ Script.js
@@ -0,0 +1,11 @@
+let body = document.getElementById('body')
+let titulo = document.getElementById('titulo');
+let paragrafo = document.getElementById('Paragrafo');
+function mudartema(){
+    body.classList.add("dark");
+}
+
+function retornatema(){
+    body.classList.remove("dark");
+ 
+    };
